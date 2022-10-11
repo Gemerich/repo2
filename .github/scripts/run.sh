6ae2aeb862429204c0e91213e4665c6a02f2746b
Gustavo Emerich <gustavo@diviproject.org>
2022-10-11 19:38:22 -0300

    qwdqwdqw

--- .github/scripts/run.sh
+++ .github/scripts/run.sh
@@ -1,5 +1,5 @@
 # echo the new version number
-newversion=$2
+newversion=$1
 echo "new version: ${newversion}"
 
 search='("version":[[:space:]]*").+(")'
@@ -7,9 +7,9 @@ replace="\1${newversion}\2"
 
 sed -i -E "s/${search}/${replace}/g" "package.json"
 
-git config user.name github-actions
-git config user.email github-actions@github.com
-git add .
-git commit -m "Bump to ${newversion}"
-git tag "v-${1}-release-${newversion}"
-git push origin --tags
+# git config user.name github-actions
+# git config user.email github-actions@github.com
+# git add .
+# git commit -m "Bump to ${newversion}"
+# git tag "v-${1}-release-${newversion}"
+# git push origin --tags
